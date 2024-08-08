286562b14108f38403792504119e34a090dfbab5
Silver094 <deepanshuvishwakarma14@gmail.com>
2024-08-08 14:28:38 +0530

    Mogoose update

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const DB = process.env.DATABASE
+const DB = process.env.DATABASE;
+
+console.log('Connecting to:', DB);  // Log the connection string
 
 mongoose.set('strictQuery', false);
-mongoose.connect(DB).then(() => {
-    console.log('Connection Successfull');
-}).catch((err) => {
-    console.log(err);
-})
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connection Successful');
+    })
+    .catch((err) => {
+        console.error('Connection error:', err);
+    });
